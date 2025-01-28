248589626bd9dbf0d7269ece6141daf03c99c63c
Alisha <alisha.kausheen@hashicorp.com>
2025-01-28 18:41:41 +0530

    browser testing

---
+++ 11-browsertest1.js
@@ -0,0 +1,29 @@
+//taking screenshot
+import { browser } from 'k6/browser';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
+};
+
+export default async function () {
+  const page = await browser.newPage();
+
+  try {
+    await page.goto('https://www.google.com/search?q=full+form+of+rpc&oq=&gs_lcrp=EgZjaHJvbWUqCQgCEEUYOxjCAzIJCAAQRRg7GMIDMgkIARBFGDsYwgMyCQgCEEUYOxjCAzIJCAMQRRg7GMIDMgkIBBBFGDsYwgMyCQgFEEUYOxjCAzIJCAYQRRg7GMIDMgkIBxBFGDsYwgPSAQsxNTgyNzE4ajBqN6gCCLACAQ&sourceid=chrome&ie=UTF-8');
+    await page.screenshot({ path: 'screenshots/screenshot.png' });
+  } finally {
+    await page.close();
+  }
+}
